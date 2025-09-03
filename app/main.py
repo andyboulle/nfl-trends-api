12eccffe68f301c6e82f59c5cf911e7d37a6db8d
Andy Boulle <andyboulle@gmail.com>
2025-09-02 20:49:56 -0400

    updated CORS policy

--- app/main.py
+++ app/main.py
@@ -11,7 +11,13 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000", 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://nfl-trends-ui.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
