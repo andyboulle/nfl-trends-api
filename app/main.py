bcb37d54a0164c15c35a4d92b68ae8b490be5a50
Andy Boulle <andyboulle@gmail.com>
2025-09-04 14:58:05 -0400

    added new domain to CORS, updated fly toml to keep machines on

--- app/main.py
+++ app/main.py
@@ -24,7 +24,8 @@ app.add_middleware(
         "http://127.0.0.1:3000", 
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "https://nfl-trends-ui.vercel.app"
+        "https://nfl-trends-ui.vercel.app",
+        "https://nfltrend.com"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
